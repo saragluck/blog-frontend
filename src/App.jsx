d2458678a2a884af0711ad0de049ef55757cb476
Sara Gluck <srfgglck@gmail.com>
2022-12-08 12:38:59 -0600

    Adds sample blog posts

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,31 @@ function App() {
 
       <div id="posts-index">
         <h1>All posts</h1>
+        <div className="posts">
+          <h2>My First Blog Post</h2>
+          <p> Hi, I'm the first blog post!</p>
+          <img src="https://static.frame.work/aj192kv9jkobd4std75kia0vv100" alt="" />
+        </div>
+        <div className="posts">
+          <h2>My Second Blog Post</h2>
+          <p> Hi, I'm the second blog post bla bla bla bla bla bla bla </p>
+          <img
+            src="https://cb2.scene7.com/is/image/CB2/WarnerTableLampShF16/$web_pdp_main_carousel_sm$/190905021327/warner-table-lamp.jpg"
+            alt=""
+          />
+        </div>
+        <div className="posts">
+          <h2>My Third Blog Post</h2>
+          <p> Hi, I'm the third blog post more bla bla bla bla bla bla bla </p>
+          <img
+            src="https://www.meijer.com/content/dam/meijer/product/0002/40/0005/98/0002400005988_1_A1C1_0600.png"
+            alt=""
+          />
+        </div>
       </div>
 
       <footer>
-        <p>Copyright 20XX</p>
+        <p>Copyright 2022</p>
       </footer>
     </div>
   );
