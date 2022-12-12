9a16ea8495f1da17cbca1eb57a1440bfdbae41a7
Sara Gluck <srfgglck@gmail.com>
2022-12-12 12:45:50 -0600

    Makes the modal show product info

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,13 +7,14 @@ import { Modal } from "./Modal";
 export function Home() {
   const [posts, setPosts] = useState([]);
   const [isPostsShowVisible, setIsPostsShowVisible] = useState(false);
-  const handleShowPost = () => {
+  const handleShowPost = (post) => {
     setIsPostsShowVisible(true);
+    setCurrentPost(post);
   };
   const handleHidePost = () => {
     setIsPostsShowVisible(false);
   };
-
+  const [currentPost, setCurrentPost] = useState({});
   const handleIndexPosts = () => {
     console.log("Get the posts");
     axios.get("http://localhost:3000/posts.json").then((response) => {
@@ -24,9 +25,10 @@ export function Home() {
   useEffect(handleIndexPosts, []);
   return (
     <div>
-      <button onClick={handleShowPost}>GIMME DAT MODAL!</button>
+      <PostsIndex posts={posts} onSelectPost={handleShowPost} />
       <Modal show={isPostsShowVisible} onClose={handleHidePost}>
-        Test
+        <h2>{currentPost.title}</h2>
+        <h3>{currentPost.body}</h3>
       </Modal>
       <PostsNew />
       <PostsIndex posts={posts} />
--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -2,12 +2,13 @@ export function PostsIndex(props) {
   console.log(props);
   return (
     <div id="posts-index">
-      <h1>All Recipes!!</h1>
+      <h1>All Posts!!</h1>
       {props.posts.map((post) => (
         <div key={post.id} className="posts">
           <h2>{post.title}</h2>
           <h4>{post.body}</h4>
           <img src={post.image} alt="" />
+          <button onClick={()=> props.onSelectPost(post)}>More Info!</button>
         </div>
       ))}
     </div>
