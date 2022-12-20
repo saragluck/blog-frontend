ebc3df57e9c38af485b0800dc391268f5563c8ba
Sara Gluck <srfgglck@gmail.com>
2022-12-20 12:42:31 -0600

    Adds cat error

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -6,6 +6,8 @@ export function Signup() {
 
   const [name, setName] = useState("");
 
+  const [status, setStatus] = useState(undefined);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     setErrors([]);
@@ -18,6 +20,7 @@ export function Signup() {
         window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
       })
       .catch((error) => {
+        setStatus(error.response.status);
         console.log(error.response.data.errors);
         setErrors(error.response.data.errors);
       });
@@ -27,36 +30,37 @@ export function Signup() {
     <div>
       <div id="signup">
         <h1>Signup</h1>
-      </div>
-      <ul>
-        {errors.map((error) => (
-          <li key={error}>{error}</li>
-        ))}
-      </ul>
+        {status ? <img src={`https://http.cat/${status}`} alt="" /> : null}
+        <ul>
+          {errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          Name:{" "}
-          <input
-            name="name"
-            className="form-control"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-            type="text"
-          />
-        </div>
-        <small>{20 - name.length} characters remaining</small>
-        <div>
-          Email: <input name="email" className="form-control" type="email" />
-        </div>
-        <div>
-          Password: <input name="password" className="form-control" type="password" />
-        </div>
-        <div>
-          Password confirmation: <input name="password_confirmation" className="form-control" type="password" />
-        </div>
-        <button type="submit">Signup</button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <div>
+            Name:{" "}
+            <input
+              name="name"
+              className="form-control"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              type="text"
+            />
+          </div>
+          <small>{20 - name.length} characters remaining</small>
+          <div>
+            Email: <input name="email" className="form-control" type="email" />
+          </div>
+          <div>
+            Password: <input name="password" className="form-control" type="password" />
+          </div>
+          <div>
+            Password confirmation: <input name="password_confirmation" className="form-control" type="password" />
+          </div>
+          <button type="submit">Signup</button>
+        </form>
+      </div>
     </div>
   );
 }
